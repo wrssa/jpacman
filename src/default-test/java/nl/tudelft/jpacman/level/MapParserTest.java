6e010f253e351dcbbb315c17a2a4107781915557
wrssa <warisa.kon@student.mahidol.ac.th>
2023-11-20 20:21:58 +0700

    testParseMapGood()
    ITCS473 Exercise - JPacman 5
    - build.gradle updated
    - Create new MapParserTest.java file
    - New testParseMapGood() test method

---
+++ src/default-test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -0,0 +1,47 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * This is a test class for MapParser.
+ */
+@ExtendWith(MockitoExtension.class)
+public class MapParserTest {
+    @Mock
+    private BoardFactory boardFactory;
+    @Mock
+    private LevelFactory levelFactory;
+    @Mock
+    private Blinky blinky;
+
+    /**
+     * Test for the parseMap method (good map).
+     */
+    @Test
+    public void testParseMapGood() {
+        MockitoAnnotations.initMocks(this);
+        assertNotNull(boardFactory);
+        assertNotNull(levelFactory);
+        Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
+        MapParser mapParser = new MapParser(levelFactory, boardFactory);
+        ArrayList<String> map = new ArrayList<>();
+        map.add("############");
+        map.add("#P        G#");
+        map.add("############");
+        mapParser.parseMap(map);
+        Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
+    }
+
+}
+
